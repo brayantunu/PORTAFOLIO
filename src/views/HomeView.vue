70298ddd6f4588a30ce2575185de2e71bd59bd92
brayantunu <bstunubala@misena.edu.co>
2023-07-01 21:08:52 -0500

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+<NavBar/>
+<EstudiosVue/
+</template>
+
+<script>
+// @ is an alias to /src
+import NavBar from '../components/NavBar.vue'
+import Estudios from '@/components/Estudios.vue';
+export default {
+  name: 'HomeView',
+  components: {
+    NavBar
+    
+  }
+}
+</script>
